6a092b653c01346452fc5deba2eb5ac6ddcaf049
philippe <philippe117@live.fr>
2020-01-10 09:45:34 -0500

    Add more verbose and safety to the install file.

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
-git clone https://github.com/resibots/libdynamixel && \
-cd libdynamixel &&\
-./waf configure --prefix ~/libdynamixel &&\
+#!/usr/bin/env bash
+##
+## This script is intended to be automatically run by catkin when compiling.
+##
+
+repositoryName="libdynamixel"
+installPath="~/libdynamixel"
+defaultBranch="x-series"
+
+if [ ! -d $repositoryName ]; then
+    echo -e "\n*** Cloning libdynamixel ***"
+    git clone --single-branch --branch $defaultBranch https://github.com/resibots/libdynamixel
+else
+    echo "*** libdynamixel is already cloned"
+fi
+
+echo -e "\n*** Installing libdynamixel ***"
+cd $repositoryName &&\
+git pull origin $defaultBranch
+./waf configure --prefix $installPath &&\
 ./waf && ./waf install
